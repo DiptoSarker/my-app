2c661f5c0bf42b826daaf198ec8dfb8a4b83ca15
Dipto Sarker <diptosarker802@gmail.com>
2022-11-08 13:15:21 +0600

    first commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import { adapter } from 'sveltekit-adapter-aws';
+import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-    kit: {
-        adapter: adapter(),
-        prerender: {
-            default: true
-        }
-    }
+export default {
+  preprocess: preprocess(),
+  kit: {
+    adapter: adapter({
+      autoDeploy: true,
+    }),
+  },
 };
-
-export default config;
